2ea4fea6a14a89ec88b0c26516756f0bd6b69a6c
public-release <as-public-release@janestreet.com>
2024-08-13 20:53:44 +0100

    v0.17~preview.129.36+325

--- src/tilde_f.ml
+++ src/tilde_f.ml
@@ -1,13 +1,13 @@
 open! Base
 
 include Monad.Make_indexed (struct
-  type ('a, 'i, 'j) t = f:('a -> 'j) -> 'i
+    type ('a, 'i, 'j) t = f:('a -> 'j) -> 'i
 
-  let return x ~f = f x
-  let bind t ~f:bind_f ~f = t ~f:(fun x -> bind_f x ~f)
-  let map t ~f:map_f ~f = t ~f:(fun x -> f (map_f x))
-  let map = `Custom map
-end)
+    let return x ~f = f x
+    let bind t ~f:bind_f ~f = t ~f:(fun x -> bind_f x ~f)
+    let map t ~f:map_f ~f = t ~f:(fun x -> f (map_f x))
+    let map = `Custom map
+  end)
 
 let run t = t ~f:Fn.id
 let of_curried t ~f = t ~f:(fun a b -> f (a, b))
